560697ee2a972408ba5fe572c1b9eb080d72b1ba
Sergey <stalkerzoff@gmail.com>
2019-02-23 20:11:11 +0200

    add input handlers  to the admin component

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -3,17 +3,46 @@ import PropTypes from 'prop-types'
 
 export default class Admin extends Component {
   static propTypes = {
-    prop: PropTypes
+    title: PropTypes.string,
+  }
+
+  state = {
+    title: '',
+    category: '',
+    text: '',
+    image: '',
+  }
+
+  handleSubmit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  handleTitleChange = (event) => {
+    this.setState({ title: event.target.value, });
+  }
+
+  handleCategoryChange = (event) => {
+    this.setState({ category: event.target.value });
+  }
+
+  handleTextChange = (event) => {
+    this.setState({ text: event.target.value });
+  }
+
+  handleImageChange = (event) => {
+    this.setState({ image: event.target.files[0] });
   }
 
   render() {
+    const { title, category, text, image } = this.state;
     return (
       <div className="container add-news">
         <h1 className="add-news__header">Добавить новость</h1>
-        <form className="add-news__form">
+        <form className="add-news__form" onSubmit={this.handleSubmit}>
           <div className="add-news__title">
             <label className="add-news__title-label" htmlFor="title">Заголовок: </label>
-            <input className="add-news__title-input" type="text" name="newstitle" id="title" />
+            <input className="add-news__title-input" type="text" name="newstitle" id="title" value={title} onChange={this.handleTitleChange} />
           </div>
 
           {/* <select id="categoryselect" name="newscategoryselect">
@@ -24,17 +53,17 @@ export default class Admin extends Component {
 
           <div className="add-news__category">
             <label className="add-news__category-label" htmlFor="categorytext">Категория:</label>
-            <input className="add-news__category-input" type="text" name="newscategorytext" id="categorytext" />
+            <input className="add-news__category-input" type="text" name="newscategorytext" id="categorytext" value={category} onChange={this.handleCategoryChange} />
           </div>
 
           <div className="add-news__text">
             <label className="add-news__text-label" htmlFor="text">Текст новости:</label>
-            <textarea className="add-news__text-textarea" name="newstext" id="text" ></textarea>
+            <textarea className="add-news__text-textarea" name="newstext" id="text" value={text} onChange={this.handleTextChange} ></textarea>
           </div>
 
           <div className="add-news__img">
             <label className="add-news__img-label" htmlFor="file">Изображение:</label>
-            <input type="file" name="newsimage" id="file" />
+            <input type="file" name="newsimage" id="file" onChange={this.handleImageChange} />
           </div>
 
           {/* <div className="add-news__main">
@@ -42,9 +71,10 @@ export default class Admin extends Component {
             <input className="add-news__main-checkbox" type="checkbox" name="newsmain" id="main" />
           </div> */}
 
-          <input className="add-news__submit-btn" type="submit" value="Добавить новость" />
+          <input className={`add-news__submit-btn ${(title && category && text && image) ? null : 'add-news__submit-btn--disabled'}`} type="submit" value="Добавить новость" disabled={!(title && category && text && image)} />
         </form>
       </div>
     )
   }
 }
+

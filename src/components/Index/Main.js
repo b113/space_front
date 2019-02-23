3b3b8b2279c0b0d60277ea30a48c6e1862262f6c
Maxim <k.maxim107@gmail.com>
2019-02-23 20:18:55 +0200

    fix bugs

--- src/components/Index/Main.js
+++ src/components/Index/Main.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import {fetchPosts} from '../../redux/actions/common';
 import {connect} from 'react-redux';
 import HeroBlock from './components/HeroBlock';
 import Article from './components/Article';
-import {Header} from '../Common/index';
+import {Header, Footer} from '../Common/index';
 import uuid from 'uuid/v1';
 
-class Main extends Component {
+class Main extends PureComponent {
   static propTypes = {
     dispatch: PropTypes.func,
     articles : PropTypes.object,
@@ -29,30 +29,29 @@ class Main extends Component {
     const lastArticle = articles.data && articles.data.slice(from,-1)[0];
     return (
       <div className='main_page'> 
-        <div className='container menu-container'>
-          <Header />
-        </div>
-        <div className='container'>
-            {lastArticle && 
-                <HeroBlock 
-                    title={lastArticle.name} 
-                    description={lastArticle.model}
-                    image={lastArticle.model} 
-                />
-            }
-            <div className="article_wrap">
-            {
-                articles.data && 
-                articles.data.length > 0 && 
-                articles.data.map((el)=> 
-                    <Article 
-                        key={uuid()} 
-                        title={el.name} 
-                        description={el.model}
-                        image={el.model}
-                    />)
-            }
-            </div>
+        <div className='main-container'>
+          <div className='container'>
+              {lastArticle && 
+                  <HeroBlock 
+                      title={lastArticle.name} 
+                      description={lastArticle.model}
+                      image={lastArticle.model} 
+                  />
+              }
+              <div className="article_wrap">
+              {
+                  articles.data && 
+                  articles.data.length > 0 && 
+                  articles.data.map((el)=> 
+                      <Article 
+                          key={uuid()} 
+                          title={el.name} 
+                          description={el.model}
+                          image={el.model}
+                      />)
+              }
+              </div>
+          </div>
         </div>
       </div>
     )

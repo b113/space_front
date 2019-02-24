79601e011ddcb76af45219efd26c78ac2731f6ab
hillel.hackaton@gmail.com <hillel.hackaton@gmail.com>
2019-02-24 09:36:25 +0200

    build

---
+++ build/precache-manifest.2f7944e2f223b893098f2c8d9262a0a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "93480927954d4cd01fa1",
+    "url": "/static/js/main.93480927.chunk.js"
+  },
+  {
+    "revision": "f8e18c214832ae28796b",
+    "url": "/static/js/2.f8e18c21.chunk.js"
+  },
+  {
+    "revision": "93480927954d4cd01fa1",
+    "url": "/static/css/main.eb284433.chunk.css"
+  },
+  {
+    "revision": "414970edb17d227d6f290fed0d58ada3",
+    "url": "/index.html"
+  }
+];
